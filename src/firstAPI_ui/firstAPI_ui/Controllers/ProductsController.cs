79ffc683f23e56b9c52fc7cffed67efd02b357e2
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-07-20 16:15:29 +0300

    dependency injection

---
+++ src/firstAPI_ui/firstAPI_ui/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using firstAPI_ui.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace firstAPI_ui.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var products = new List<Product>() 
+            {
+                new Product() { Id = 1, Name = "Bilgisayar"},
+                new Product() { Id = 2, Name = "Tablet"},
+                new Product() { Id = 3, Name = "Telefon"}
+            };
+            return Ok(products);
+        }
+    }
+}
--- src/firstAPI_ui/firstAPI_ui/Program.cs
+++ src/firstAPI_ui/firstAPI_ui/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using firstAPI_ui.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +8,7 @@ builder.Services.AddControllers(); //controller'ları ekledik
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer(); //controller'ı keşfetmesi için
 builder.Services.AddSwaggerGen();//swagger yapısı inşa edildi
+
 
 var app = builder.Build();//bize bir web application verecek
 
