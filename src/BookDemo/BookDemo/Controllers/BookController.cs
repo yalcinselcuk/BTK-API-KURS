cfe0ff1f571ae92044871b0161e4dfc958ec9897
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-07-20 18:09:46 +0300

    CRUD

---
+++ src/BookDemo/BookDemo/Controllers/BookController.cs
@@ -0,0 +1,78 @@
+﻿using BookDemo.Data;
+using BookDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+        //private IList<Book> listBooks;
+        public BookController()
+        {
+            //listBooks = new List<Book>() 
+            //{
+            //    new Book() { Id = 1, Title = "Suç ve Ceza", Price = 100},
+            //    new Book() { Id = 2, Title = "Sefiller", Price = 90},
+            //    new Book() { Id = 3, Title = "Romeo ve Juliet", Price = 105}
+            //};
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBook ()
+        {
+            var books = BookDbContext.listBooks.ToList();
+            return Ok(books);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetBookById (int id)
+        {
+            var book = BookDbContext.listBooks.FirstOrDefault(b => b.Id == id);
+            if(book != null)
+            {
+                return Ok(book);
+            }
+            return BadRequest("Geçersiz ID değeri girdiniz");
+        }
+        [HttpPost("[action]")]
+        public IActionResult CreateBook (Book _book) 
+        {
+            var book = _book; 
+            if(book.Title == "" || book.Title == "string")
+            {
+                return BadRequest("Title'a Değer Girilmelidir");
+            }
+            BookDbContext.listBooks.Add(book);
+            return Ok(book);
+        }
+        [HttpPut("[action]")]
+        public IActionResult UpdateBook (int id,Book _book) 
+        {
+            var book = BookDbContext.listBooks.FirstOrDefault(b=> b.Id == id);
+            if(book == null)
+            {
+                return NotFound("Obje Bulunamadı");
+            }
+            book.Title = _book.Title;
+            book.Price = _book.Price;
+            return Ok(book);
+
+        }
+        [HttpDelete("[action]")]
+        public IActionResult DeleteBook (int id)
+        {
+            var book = BookDbContext.listBooks.FirstOrDefault(b => b.Id == id);
+            if(book == null)
+            {
+                return NotFound("Girdiğiniz Id'ye Ait Kitap Bulunamadı");
+            }
+            BookDbContext.listBooks.RemoveAt(id-1);
+            return Ok(book);
+
+        }
+    }
+}
---
+++ src/BookDemo/BookDemo/Data/BookDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BookDemo.Models;
+
+namespace BookDemo.Data
+{
+    public static class BookDbContext
+    {
+        public static IList<Book> listBooks;
+
+        static BookDbContext()
+        {
+            listBooks = new List<Book>()
+            {
+                new Book() { Id = 1, Title = "Suç ve Ceza", Price = 100},
+                new Book() { Id = 2, Title = "Sefiller", Price = 90},
+                new Book() { Id = 3, Title = "Romeo ve Juliet", Price = 105}
+            };
+        }
+    }
+}
