ed2f5848d62d41335c73bf26f08df30f76af0670
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-07-22 22:50:09 +0300

    Repository için DbContext bağlantısı ve tablo için DbSet kullanımı

---
+++ src/StoreApp/StoreApp.API/Repositories/BookDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreApp.API.Models;
+
+namespace StoreApp.API.Repositories
+{
+    public class BookDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
