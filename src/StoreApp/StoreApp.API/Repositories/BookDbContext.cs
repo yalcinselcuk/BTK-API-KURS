a2f725746946a004c1995c745ce327212cd5e75b
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-07-22 23:27:27 +0300

    Db bağlantısı için repository'de cons tanımlandı, appsettings'de db yolu eklendi ve program.cs'de conf yapıldı

--- src/StoreApp/StoreApp.API/Repositories/BookDbContext.cs
+++ src/StoreApp/StoreApp.API/Repositories/BookDbContext.cs
@@ -6,5 +6,7 @@ namespace StoreApp.API.Repositories
     public class BookDbContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
+
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) { }
     }
 }
