da5a60225ce8c184ae8345bf70475277515ce05f
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-07-25 21:24:12 +0300

    exception için extension yazıldı

---
+++ src/StoreApp/src/WebAPI/StoreApp.API/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using StoreApp.Entities.ErrorModel;
+using StoreApp.Services;
+using System.Net;
+
+namespace StoreApp.API.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this WebApplication app, ILoggerService logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        logger.LogError($"Bir şeyler ters gitti : {contextFeature.Error}");
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "internal server error"
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
